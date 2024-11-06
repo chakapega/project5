1ac7811297228b6be9290752c246b69a61ca9287
Vadim Marchenko <v3.marchenko.itransition.com@admitad.com>
2024-11-06 14:19:43 +0300

    Add search city form

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { useState } from "react";
 import styles from "./header.module.scss";
 
 export default function Header() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearchInputChange = (event: { target: { value: string } }) => setSearchValue(event.target.value);
+
   return (
-    <header className={styles.header}>Header</header>
+    <header className={styles.header}>
+      <form>
+        <input value={searchValue} onChange={handleSearchInputChange} type="text" placeholder="City" className={styles.searchInput} />
+        <button type="submit" className={styles.searchButton}>Search</button>
+      </form>
+    </header>
   )
 }
